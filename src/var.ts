4e1f888caf272e51adddda683d30a78c6ed26670
josejooj <josejooj.dev@gmail.com>
2024-07-03 08:14:07 -0300

    feat: new function to set language

--- src/var.ts
+++ src/var.ts
@@ -1,3 +1,5 @@
-type Languages = 'pt_BR' | 'en_US';
-
-export let ZENNIFY_API_RESPONSE_LANGUAGE: Languages = 'en_US';
+export type SupportedLanguages = 'pt_BR' | 'en_US';
+export let ZENNIFY_API_RESPONSE_LANGUAGE: SupportedLanguages = 'en_US';
+export function SetAPIResponseLanguage(lang: SupportedLanguages) {
+    return ZENNIFY_API_RESPONSE_LANGUAGE = lang;
+}
