dd64ee7c00f28ba4ad2c819471c029cfa754bc92
josejooj <josejooj.dev@gmail.com>
2024-08-03 13:34:16 -0300

    fix: fix typing for ImageSources

--- src/images.ts
+++ src/images.ts
@@ -1,7 +1,13 @@
-type ImageSources = 'void' | 'null' | 'empty' | 'product_icon' | 'general'
+interface ImageSources {
+    void: void,
+    null: null,
+    empty: string,
+    product_icon: string,
+    general: string
+}
 
 const template = `https://cdn.zennify.app/media/{store_id}/{id}.png`
-const sources: Record<ImageSources, string | void | null> = {
+const sources: Record<keyof ImageSources, ImageSources[keyof ImageSources]> = {
     'product_icon': `https://zennify.app/assets/images/emojis/caixa.png`,
     'general': `https://zennify.app/assets/images/icon.png`,
     'void': void 0,
@@ -9,8 +15,12 @@ const sources: Record<ImageSources, string | void | null> = {
     'empty': ""
 }
 
-export function GetImageURL(source: ImageSources, store_id?: number | null, id?: number | null) {
-    if (!store_id || !id) return sources[source];
+export function GetImageURL<Source extends keyof ImageSources>(
+    source: Source,
+    store_id?: number | null,
+    id?: number | null
+) {
+    if (!store_id || !id) return sources[source] as ImageSources[Source];
     else return template
         .replace("{store_id}", store_id.toString())
         .replace("{id}", id.toString());
