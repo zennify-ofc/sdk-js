1f3f96b8d65732e7dc789530eeeb54da3584033b
josejooj <josejooj.dev@gmail.com>
2024-08-03 14:12:03 -0300

    perf: remove product statistics

--- src/interfaces/Products.ts
+++ src/interfaces/Products.ts
@@ -14,8 +14,8 @@ export interface Product {
     created_at: string,
     store_id: number,
     owner_id: number,
-    icon_id: null | number,
-    banner_id: null | number,
+    icon_id?: null | number,
+    banner_id?: null | number,
     value: number,
     description: {
         short: null | string,
@@ -25,11 +25,5 @@ export interface Product {
     stock: {
         size: number,
         locked: boolean
-    },
-    statistics: {
-        revenue_total: number,
-        revenue_daily: number,
-        revenue_weekly: number,
-        revenue_monthly: number
     }
 }
