e472a45eb32e7c40da0a542fd9a04aa661004f2f
josejooj <josejooj.dev@gmail.com>
2024-08-03 13:27:06 -0300

    feat: add void, null and empty to GetImageURL

--- src/images.ts
+++ src/images.ts
@@ -1,9 +1,12 @@
-type ImageSources = 'product_icon' | 'general'
+type ImageSources = 'void' | 'null' | 'empty' | 'product_icon' | 'general'
 
 const template = `https://cdn.zennify.app/media/{store_id}/{id}.png`
-const sources: Record<ImageSources, string> = {
+const sources: Record<ImageSources, string | void | null> = {
     'product_icon': `https://zennify.app/assets/images/emojis/caixa.png`,
-    'general': `https://zennify.app/assets/images/icon.png`
+    'general': `https://zennify.app/assets/images/icon.png`,
+    'void': void 0,
+    'null': null,
+    'empty': ""
 }
 
 export function GetImageURL(source: ImageSources, store_id?: number | null, id?: number | null) {
