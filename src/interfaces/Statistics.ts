3dfe62c0ae238d689fec8ecb9a973ac7c1b3b49e
josejooj <josejooj.dev@gmail.com>
2025-01-23 17:11:22 +0000

    feat: add customers to statistics

--- src/interfaces/Statistics.ts
+++ src/interfaces/Statistics.ts
@@ -2,7 +2,12 @@ export type Statistic = {
     count: number,
     users: number,
     value: number,
-    rating: number
+    rating: number,
+    customers: Array<{
+        id: number,
+        discord_user_id: string,
+        value: number
+    }>
 }
 
 export type Statistics = Record<string, Record<number, Statistic>>
