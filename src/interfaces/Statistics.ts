3372954eb67f83cd5a937eb9baae4f817b95ad44
josejooj <josejooj.dev@gmail.com>
2025-05-18 15:02:19 -0300

    refactor: remove customers from statistics

--- src/interfaces/Statistics.ts
+++ src/interfaces/Statistics.ts
@@ -2,12 +2,7 @@ export type Statistic = {
     count: number,
     users: number,
     value: number,
-    rating: number,
-    customers: Array<{
-        id: number,
-        discord_user_id: string,
-        value: number
-    }>
+    rating: number
 }
 
 export type Statistics = Record<string, Record<number, Statistic>>
