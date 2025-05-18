b77bb1e3f19ba12931e27352bdc4c6de1eb117f6
josejooj <josejooj.dev@gmail.com>
2025-05-18 18:22:17 -0300

    refactor: statistics

--- src/interfaces/Statistics.ts
+++ src/interfaces/Statistics.ts
@@ -1,22 +1,21 @@
-export type Statistic = {
-    count: number,
-    users: number,
-    value: number,
-    rating: number
-}
-
-export type Statistics = Record<string, Record<number, Statistic>>
-export type StatisticsResponse = {
-    last_update: string,
-    created_at: string
-    statistics: Statistics
-};
-
-export type Ranking = {
+export type ProductStatisticsCustomer = {
     id: number,
     discord_user_id: string,
     revenue: number,
     sales: number
 };
 
-export type RankingResponse = Ranking[]
+export type ProductStatistics = {
+    count: number,
+    users: number,
+    value: number,
+    rating: number,
+    customers: ProductStatisticsCustomer
+}
+
+export type Statistics = Record<string, Record<number, ProductStatistics>>
+export type StatisticsResponse = {
+    last_update: string,
+    created_at: string
+    statistics: Statistics
+};
