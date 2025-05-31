292488880880d6699f268c45c37350d68d1d0e38
josejooj <josejooj.dev@gmail.com>
2025-05-31 05:43:03 -0300

    feat: new ZennifySDK class

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,9 @@
+import { GetImageURL } from './images';
+
 export * from './var';
 export * from './errors';
 export * from './utils';
 export * from './RequestError';
-export * from './images'
 
 export * from './interfaces/Panels';
 export * from './interfaces/Products';
@@ -10,3 +11,27 @@ export * from './interfaces/Guild';
 export * from './interfaces/Stores';
 export * from './interfaces/Transactions';
 export * from './interfaces/Statistics';
+
+type SDKOptions = {
+    domain?: string,
+    // token?: string
+}
+
+export class ZennifySDK {
+
+    private readonly domain: string;
+    // private readonly token: string | null = null;
+    // private readonly user_agent: string = "Zennify-sdk";
+
+    constructor(options?: SDKOptions) {
+        this.domain = options?.domain ?? "zennify.app";
+        // this.token = options?.token ?? null;
+    }
+
+    media = {
+        get_url: async (...options: Parameters<typeof GetImageURL>) => {
+            return GetImageURL(...options)?.replace("zennify.app", this.domain)
+        }
+    }
+
+}
