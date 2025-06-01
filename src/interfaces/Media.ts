a48c2c6d0e0c8d88b438cb5a6da335409a3808c5
josejooj <josejooj.dev@gmail.com>
2025-06-01 15:36:29 -0300

    fix: typing

--- src/interfaces/Media.ts
+++ src/interfaces/Media.ts
@@ -1,5 +1,5 @@
 export type MediaUpdatePayload = {
-    store?: ("icon" | "banner" | "banner_discord_sale_message" | "background_ranking")[],
+    store?: ("icon" | "banner" | "banner_sale_approved" | "background_ranking")[],
     products?: Record<string | number, ("icon" | "banner")[]>,
     discord_sale_panels?: Record<string | number, ("icon" | "banner")[]>
 }
