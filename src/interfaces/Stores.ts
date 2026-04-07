47c7bbbe873e0c20633f69a75addc7111aaf9b69
josejooj <josejooj.dev@gmail.com>
2026-04-07 20:00:31 -0300

    refactor: stores  typing

--- src/interfaces/Stores.ts
+++ src/interfaces/Stores.ts
@@ -1,14 +1,12 @@
-import { DiscordGuild } from "./Guild";
-
 export type Addons = 'custom_bot' | 'divulgation' | 'antiraid' | 'managed_automod';
 
 export interface PartialStore {
     id: number,
     name: string,
     expires_at: number,
+    created_at?: boolean | null,
     icon_id?: string | null,
     banner_id?: string | null,
-    pending_setup?: boolean | null,
     is_owner: boolean
 }
 
@@ -19,7 +17,6 @@ export type StoreModeratorPermissions =
     | 'MANAGE_STORE'          // Gerenciar a loja (Editar bot, nome, imagem, bancos bloqueados) e comandos /customize, /mod, /autorole, /temprole
     | 'MANAGE_SALES'          // Gerenciar vendas (Aprovar, cancelar e reembolsar)
     | 'VIEW_STATISTICS'       // Ver estatísticas (Acessar a home da dashboard, usar o comando /ranking e ver estatisticas de produtos)
-
 
 export type StoreModerator = {
     username: string,
@@ -40,30 +37,18 @@ export interface FullStore {
     background_ranking_id: string | null,
     addons: Addons[],
     blocked_banks: string[],
-    pending_setup?: boolean | null,
     limit_product: number,
     limit_discord_server: number,
-    discord_channels: Record<
-        string /** guild id */,
-        Record<
-            'sale' | 'voice' | 'feedback' | 'carts',
-            string | null | void /** channel id */
-        >
-    >,
     discord_bot: {
         id: string
-        token: string | null
         cluster: string | null
     },
-    moderators: Record<string, StoreModerator>,
-    discord_bot_guilds: Record<string, DiscordGuild>
+    moderators: Record<string, StoreModerator>
 }
 
 export type StoreInvite = {
-    invite: {
-        permissions: StoreModeratorPermissions[],
-        expires: number
-    },
+    permissions: StoreModeratorPermissions[],
+    expires: number
     store: {
         name: string,
         created_at: number
