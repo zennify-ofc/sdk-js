b6221afd60528571270e20056685d5a3cab94744
josejooj <josejooj.dev@gmail.com>
2026-04-09 01:25:39 -0300

    fix: panel properties

--- src/interfaces/Panels.ts
+++ src/interfaces/Panels.ts
@@ -1,6 +1,5 @@
 export interface PanelProduct {
     id: number,
-    position: number,
     emoji?: string | null,
     label?: string | null
 }
@@ -8,9 +7,12 @@ export interface PanelProduct {
 export interface Panel {
     id: number,
     name: string,
+    created_at: number,
     placeholder: string | null,
     description: string | null,
     icon_id: string | null,
     banner_id: string | null,
+    owner_id: number,
+    store_id: number,
     products: PanelProduct[]
 }
